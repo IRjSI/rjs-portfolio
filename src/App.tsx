15e1c52bd31227b492d85365dbb66556e8866782
IRjSI <rajusuthar0077@gmail.com>
2025-11-11 14:59:15 +0530

    remove unused import for WaterDropGrid component

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Contact from "./components/Contact"
 // import { useGSAP } from "@gsap/react";
 // import gsap from "gsap";
 import Visitor from "./components/Visitor"
-import WaterDropGrid from "./components/WaterDropGrid"
 
 function App() {
 
